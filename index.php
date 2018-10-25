64966db2366c17d660dbc8667dd0b4ef4dc89296
Leonardo Santos Severino <leonardossev@gmail.com>
2018-10-25 19:10:19 -0300

    Creates helloWorld() function
    Signed-off-by: Leonardo Santos Severino <leonardossev@gmail.com>

--- index.php
+++ index.php
@@ -4,6 +4,15 @@ echo "Hello World!";
 
 print "Hello World!";
 
+echo helloWorld();
+
+print helloWorld();
+
 var_dump("Hello World!");
 
 print_r("Hello World!");
+
+function helloWorld ()
+{
+    return "Hello World!";
+}
