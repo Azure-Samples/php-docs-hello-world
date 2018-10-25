804c687f3d96f4796db49f669fbf3817d7d16adb
Leonardo Santos Severino <leonardossev@gmail.com>
2018-10-25 19:28:41 -0300

    Changes the location of the helloWorld() calling
    Signed-off-by: Leonardo Santos Severino <leonardossev@gmail.com>

--- index.php
+++ index.php
@@ -27,9 +27,6 @@ echo "Hello World!";
 
 print "Hello World!";
 
-echo helloWorld();
-
-print helloWorld();
 
 /**
  * These next functions are quite similar but also have their differences.
@@ -44,11 +41,16 @@ var_dump("Hello World!");
 
 print_r("Hello World!");
 
-var_dump(helloWorld());
-
-print_r(helloWorld());
 
 function helloWorld ()
 {
     return "Hello World!";
 }
+
+echo helloWorld();
+
+print helloWorld();
+
+var_dump(helloWorld());
+
+print_r(helloWorld());
