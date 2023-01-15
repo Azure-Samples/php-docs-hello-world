beb37adf07036b3e517534adad51f0a545a126d5
DFunnell1 <davidfunnell4@googlemail.com>
2023-01-15 19:52:15 +0000

    updated config file

--- config.php
+++ config.php
@@ -1,10 +1,7 @@
 <?php
-$hn = "df-sql-jbc.mysql.database.azure.com";
-$un = "davidf";
-$pw = "Katie090115";
-$db = "cbplearners";
 
-$conn = new mysqli($hn, $un, $pw, $db);
+
+$con = mysqli_init(); mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL); mysqli_real_connect($conn, "df-sql-jbc.mysql.database.azure.com", "davidf", "Katie090115", "cbplearners", 3306, MYSQLI_CLIENT_SSL);
 if ($conn->connect_error) die("Fatal Error");
 
 ?>
