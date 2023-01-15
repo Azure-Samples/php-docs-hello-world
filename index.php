d1d39b5335d4cdcdfcca89f1a696d04f1fe6da4c
DFunnell1 <davidfunnell4@googlemail.com>
2023-01-15 18:57:11 +0000

    a

--- index.php
+++ index.php
@@ -8,8 +8,9 @@ echo '<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="StyleSheet1.css" rel="stylesheet" />
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="Script1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    
     <title>StaffTest</title>
     
 </head>
