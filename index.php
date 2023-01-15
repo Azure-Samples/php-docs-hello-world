29fb58a1d03fa546099c76999b1d2a7925e04203
DFunnell1 <davidfunnell4@googlemail.com>
2023-01-15 19:32:17 +0000

    d

--- index.php
+++ index.php
@@ -30,19 +30,7 @@ echo '<!DOCTYPE html>
             <p>giving you access the tools you need to be the best.</p>
         </div>
         <div>';
-        require "config.php"
-        $query="SELECT * from tbl_learners;";
-                    $result = mysqli_query($conn, $query);
-	                if (!$result){
-		                echo mysqli_error($conn);
-	                }
-	                else {
-		                if(mysqli_num_rows($result)>0){
-			                while($row = mysqli_fetch_assoc($result)) {
-                                echo "{$row['firstName']}";
-                            }
-                        }
-                    }
+        
                     echo '
         </div>
     </div>
