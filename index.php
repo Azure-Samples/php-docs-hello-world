d40b2e6ebe969c4a2a939818106783330b5d6179
DFunnell1 <davidfunnell4@googlemail.com>
2023-01-15 18:51:12 +0000

    fixed missing closing script tag

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ echo '<!DOCTYPE html>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="StyleSheet1.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="Script1.js" />
+    <script src="Script1.js"></script>
     <title>StaffTest</title>
     
 </head>
