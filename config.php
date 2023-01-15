d79df345118049d77417a4b88b92c530af94c305
DFunnell1 <davidfunnell4@googlemail.com>
2023-01-15 19:56:13 +0000

    added ssl cert

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$con = mysqli_init(); mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL); mysqli_real_connect($conn, "df-sql-jbc.mysql.database.azure.com", "davidf", "Katie090115", "cbplearners", 3306, MYSQLI_CLIENT_SSL);
+$con = mysqli_init(); mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL); mysqli_real_connect($conn, "df-sql-jbc.mysql.database.azure.com", "davidf", "Katie090115", "cbplearners", 3306, MYSQLI_CLIENT_SSL);
 if ($conn->connect_error) die("Fatal Error");
 
 ?>
