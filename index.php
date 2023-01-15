5fbfb6b216fecad2973da4361372676b9ce6e425
DFunnell1 <davidfunnell4@googlemail.com>
2023-01-15 16:03:34 +0000

    added some html

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
 <?php
 
-echo "Hello World!";
+echo '<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" />
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>StaffTest</title>
+    <style>
+
+        body{
+            background-color:#E94E1B;
+        }
+        #titlediv{
+            color:#193540;
+            
+        }
+        div{
+            background-color:white;
+        }
+    </style>
+</head>
+<body>
+    <div class="container-fluid p-5">
+        <div id="titlediv" class="mt-4 p-5 rounded">
+  <h1>Jumbotron Example</h1>
+  <p>Lorem ipsum...</p>
+</div>
+    </div>
+</body>
+</html>';
+?>
