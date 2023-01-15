3cfc7e8f686750e37b5f1b43d4a009faa2c886fc
DFunnell1 <davidfunnell4@googlemail.com>
2023-01-15 18:48:27 +0000

    added nav bar

--- index.php
+++ index.php
@@ -7,28 +7,30 @@ echo '<!DOCTYPE html>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" />
-
+    <link href="StyleSheet1.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="Script1.js" />
     <title>StaffTest</title>
-    <style>
-
-        body{
-            background-color:#E94E1B;
-        }
-        #titlediv{
-            color:#193540;
-            background-color:white;
-        }
-        
-    </style>
+    
 </head>
 <body>
-    <div class="container-fluid p-5">
-        <div id="titlediv" class="mt-4 m-5 rounded">
-  <h1>Jumbotron Example</h1>
-  <p>Lorem ipsum...</p>
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#">About</a>
+  <a href="#">Services</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
 </div>
+<span onclick="openNav()">open</span>
+
+    <div id="main" class="container p-5">
+        <div id="titlediv" class="mt-4 m-5 text-center rounded">
+            <h1 class="h1">Staff test site</h1>
+            <p>giving you access the tools you need to be the best.</p>
+        </div>
+        <div
     </div>
+
 </body>
 </html>';
 ?>
