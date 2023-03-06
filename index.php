9dd05bbc2bd080e3c88afd07e80598d190f63f77
bo-taulov <127088246+bo-taulov@users.noreply.github.com>
2023-03-06 11:50:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 
-echo hash_string("test");
-
 # returns 75 character string containing
 	# SHA-256 encrypted hash created from string and random salt
 	public function hash_string($string) {
 		$salt = mcrypt_create_iv(16, MCRYPT_DEV_URANDOM);
 		return crypt($string, '$5$rounds=5000$' . $salt . '$');
 	}
+
+echo hash_string("test");
+
+
 ?>
