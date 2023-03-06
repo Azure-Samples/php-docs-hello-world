cf423cfd51e5098b0953d50994314aa831be9569
bo-taulov <127088246+bo-taulov@users.noreply.github.com>
2023-03-06 12:04:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php
-# returns 75 character string containing
-	# SHA-256 encrypted hash created from string and random salt
-	public function hash_string($string) {
-		$salt = mcrypt_create_iv(16, MCRYPT_DEV_URANDOM);
-		return crypt($string, '$5$rounds=5000$' . $salt . '$');
-	}
-echo $this->hash_string("test")
+/* These salts are examples only, and should not be used verbatim in your code.
+   You should generate a distinct, correctly-formatted salt for each password.
+*/
+echo 'Standard DES: ',
+    crypt('rasmuslerdorf', 'rl'),
+    "\n";
+echo 'Extended DES: ',
+    crypt('rasmuslerdorf', '_J9..rasm'),
+    "\n";
+echo 'MD5:          ',
+    crypt('rasmuslerdorf', '$1$rasmusle$'),
+    "\n";
+echo 'Blowfish:     ',
+    crypt('rasmuslerdorf', '$2a$07$usesomesillystringforsalt$'),
+    "\n";
+echo 'SHA-256:      ',
+    crypt('rasmuslerdorf', '$5$rounds=5000$usesomesillystringforsalt$'),
+    "\n";
+echo 'SHA-512:      ',
+    crypt('rasmuslerdorf', '$6$rounds=5000$usesomesillystringforsalt$'),
+    "\n";
 ?>
 
