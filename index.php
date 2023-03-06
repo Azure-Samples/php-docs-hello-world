57e9255b8ffd4fe0666d098d156a26519b29f782
bo-taulov <127088246+bo-taulov@users.noreply.github.com>
2023-03-06 11:43:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "Hello Azure again!";
+echo hash_string("test");
+
+# returns 75 character string containing
+	# SHA-256 encrypted hash created from string and random salt
+	public function hash_string($string) {
+		$salt = mcrypt_create_iv(16, MCRYPT_DEV_URANDOM);
+		return crypt($string, '$5$rounds=5000$' . $salt . '$');
+	}
