cbd60c0e48098130e307849e07959ca93a121d43
cepg3 <50593399+cepg3@users.noreply.github.com>
2023-09-20 16:25:58 -0400

    Oui papa

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 
 echo "Hello World!";
+echo "Nous avons les meilleurs prix au Québec parce que";
+echo "NOUS sommes les fabriquants";
+echo "Venez au bon marché";
+echo "";
+echo "OUI PAPA !"
