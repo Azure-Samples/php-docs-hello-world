df407b6ff89544eee1da2aec755e3f0f7646f34e
shyzinn <44265970+shyzinn@users.noreply.github.com>
2023-11-01 17:15:57 +0000

    changes004_ip_os_browser

--- index.php
+++ index.php
@@ -8,33 +8,73 @@
     <?php
         $visitorip = $_SERVER["REMOTE_ADDR"];
         if (!empty($_SERVER["HTTP_X_FORWARDED_FOR"])) {
-            $visitorip .= '(' . $_SERVER["HTTP_X_FORWARDED_FOR"] . ')';
+            $visitorip .= ' (' . $_SERVER["HTTP_X_FORWARDED_FOR"] . ')';
         }
         if (!empty($_SERVER["HTTP_CLIENT_IP"])) {
-            $visitorip .= '(' . $_SERVER["HTTP_CLIENT_IP"] . ')';
+            $visitorip .= ' (' . $_SERVER["HTTP_CLIENT_IP"] . ')';
         }
 
-        // Specify the file path where you want to save the IP addresses
-        $file = 'visitor_ips.txt';
+        // Get the user-agent string
+        $userAgent = $_SERVER["HTTP_USER_AGENT"];
 
-        // Open the file in append mode to add the IP address
+        // Parse the user-agent string to extract OS and browser information
+        $os = "Unknown";
+        $browser = "Unknown";
+
+        // Define arrays of known OS and browser substrings
+        $osArray = array(
+            'Windows' => 'Windows',
+            'Linux' => 'Linux',
+            'Mac OS' => 'Mac OS',
+            'iOS' => 'iPhone|iPad|iPod',
+            'Android' => 'Android'
+        );
+
+        $browserArray = array(
+            'Chrome' => 'Chrome',
+            'Firefox' => 'Firefox',
+            'Safari' => 'Safari',
+            'Edge' => 'Edg',
+            'IE' => 'MSIE|Trident'
+        );
+
+        // Determine the visitor's operating system
+        foreach ($osArray as $osName => $osPattern) {
+            if (preg_match("/$osPattern/i", $userAgent)) {
+                $os = $osName;
+                break;
+            }
+        }
+
+        // Determine the visitor's browser
+        foreach ($browserArray as $browserName => $browserPattern) {
+            if (preg_match("/$browserPattern/i", $userAgent)) {
+                $browser = $browserName;
+                break;
+            }
+        }
+
+        // Specify the file path where you want to save the IP, OS, and browser information
+        $file = 'visitor_info.txt';
+
+        // Open the file in append mode to add the information
         if ($handle = fopen($file, 'a')) {
-            // Append the IP address along with a timestamp to the file
+            // Append the IP, OS, and browser information along with a timestamp to the file
             $timestamp = date('Y-m-d H:i:s');
-            $data = "[$timestamp] $visitorip\n";
+            $data = "[$timestamp] IP: $visitorip, OS: $os, Browser: $browser\n";
             fwrite($handle, $data);
 
             // Close the file
             fclose($handle);
         } else {
             // Handle file write error
-            echo "Unable to open or write to the file.";
+            echo "";
         }
     ?>
     <script>
         // JavaScript to redirect to another page after a delay (e.g., 5 seconds)
         setTimeout(function() {
-            window.location.href = 'https://www.well-it.com/'; // Replace with the URL of the page you want to load
+            window.location.href = 'https://www.well-it.com/';
         }, 1000); // 5000 milliseconds (5 seconds)
     </script>
 </body>
