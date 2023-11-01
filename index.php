92fda2135467cad6bc716e5479d9997b711b8163
shyzinn <44265970+shyzinn@users.noreply.github.com>
2023-11-01 15:47:00 +0000

    new_testIP

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to our Landing Page</title>
+</head>
+<body>
+    <h1>Welcome to our Landing Page</h1>
 
-echo "Hello World! Now Working";
+    <?php
+    // Get the visitor's IP address
+    $ip = $_SERVER['REMOTE_ADDR'];
+
+    // Define the file to store the IP addresses
+    $filename = 'ip_log.txt';
+
+    // Open the file in append mode
+    $file = fopen($filename, 'a');
+
+    // Write the visitor's IP address to the file
+    fwrite($file, $ip . "\n");
+
+    // Close the file
+    fclose($file);
+
+    echo "Your IP address ($ip) has been saved.";
+    ?>
+</body>
+</html>
