091db4cf73c679acea0200a7ffd2de56ef96622a
shyzinn <44265970+shyzinn@users.noreply.github.com>
2023-11-01 16:35:56 +0000

    changes03_java

--- index.php
+++ index.php
@@ -6,33 +6,36 @@
 <body>
     <h1>Welcome to our Landing Page</h1>
     <?php
-$visitorip = $_SERVER["REMOTE_ADDR"];
-if (!empty($_SERVER["HTTP_X_FORWARDED_FOR"])) {
-    $visitorip .= '(' . $_SERVER["HTTP_X_FORWARDED_FOR"] . ')';
-}
-if (!empty($_SERVER["HTTP_CLIENT_IP"])) {
-    $visitorip .= '(' . $_SERVER["HTTP_CLIENT_IP"] . ')';
-}
+        $visitorip = $_SERVER["REMOTE_ADDR"];
+        if (!empty($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+            $visitorip .= '(' . $_SERVER["HTTP_X_FORWARDED_FOR"] . ')';
+        }
+        if (!empty($_SERVER["HTTP_CLIENT_IP"])) {
+            $visitorip .= '(' . $_SERVER["HTTP_CLIENT_IP"] . ')';
+        }
 
-// Specify the file path where you want to save the IP addresses
-$file = 'visitor_ips.txt';
+        // Specify the file path where you want to save the IP addresses
+        $file = 'visitor_ips.txt';
 
-// Open the file in append mode to add the IP address
-if ($handle = fopen($file, 'a')) {
-    // Append the IP address along with a timestamp to the file
-    $timestamp = date('Y-m-d H:i:s');
-    $data = "[$timestamp] $visitorip\n";
-    fwrite($handle, $data);
+        // Open the file in append mode to add the IP address
+        if ($handle = fopen($file, 'a')) {
+            // Append the IP address along with a timestamp to the file
+            $timestamp = date('Y-m-d H:i:s');
+            $data = "[$timestamp] $visitorip\n";
+            fwrite($handle, $data);
 
-    // Close the file
-    fclose($handle);
-
-    // Redirect to another page (change 'example.com' to the URL of the page you want to redirect to)
-    header("Location: https://www.well-it.com/");
-    exit; // Ensure that no other code is executed after the redirection
-} else {
-    ;
-}
-?>
+            // Close the file
+            fclose($handle);
+        } else {
+            // Handle file write error
+            echo "Unable to open or write to the file.";
+        }
+    ?>
+    <script>
+        // JavaScript to redirect to another page after a delay (e.g., 5 seconds)
+        setTimeout(function() {
+            window.location.href = 'https://www.example.com'; // Replace with the URL of the page you want to load
+        }, 1000); // 5000 milliseconds (5 seconds)
+    </script>
 </body>
 </html>
