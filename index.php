eec47ac8fbc9a8f1d7bb03323ed0bebf6f7dc036
OTA023 <133537643+OTA023@users.noreply.github.com>
2023-11-06 17:50:48 -0500

    Pedidos_06102023_1

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-
-echo "Hello World!";
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+    <h1>Seleccione un restaurante</h1>
+    <div class="restaurant-list">
+        <a href="comida/restaurante1.html">
+            <img src="images/restaurante1.jpg" alt="Restaurante 1">
+            <h2>Restaurante 1</h2>
+        </a>
+        <a href="comida/restaurante2.html">
+            <img src="images/restaurante2.jpg" alt="Restaurante 2">
+            <h2>Restaurante 2</h2>
+        </a>
+        <!-- Agrega más restaurantes aquí -->
+    </div>
+</body>
+</html>
