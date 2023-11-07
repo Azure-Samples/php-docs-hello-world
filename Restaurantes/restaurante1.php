92f936dc26a5c31e1146f75594d228e911b8beae
OTA023 <133537643+OTA023@users.noreply.github.com>
2023-11-06 19:16:00 -0500

    Restauranteycss

--- Restaurantes/restaurante1.php
+++ Restaurantes/restaurante1.php
@@ -5,19 +5,23 @@
 </head>
 <body>
     <h1>Formulario de pedido para Restaurante 1</h1>
-    <form action="procesar_pedido.php" method="post">
-        <!-- Agrega aquí los campos del formulario de pedido -->
-        <label for="nombre">Nombre:</label>
-        <input type="text" name="nombre" id="nombre" required>
-        <label for="direccion">Dirección de entrega:</label>
-        <input type="text" name="direccion" id="direccion" required>
-        <label for="comida">Seleccione su comida:</label>
-        <select name="comida" id="comida" required>
-            <option value="plato1">Plato 1</option>
-            <option value="plato2">Plato 2</option>
-            <!-- Agrega más opciones de comida aquí -->
-        </select>
-        <input type="submit" value="Enviar Pedido">
-    </form>
+    <div class="formulario">
+        <form action="../procesar_pedido.php" method="post">
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="nombre" id="nombre" required>
+
+            <label for="direccion">Dirección de entrega:</label>
+            <input type="text" name="direccion" id="direccion" required>
+
+            <label for="comida">Seleccione su comida:</label>
+            <select name="comida" id="comida" required>
+                <option value="plato1">Plato 1</option>
+                <option value="plato2">Plato 2</option>
+                <!-- Agrega más opciones de comida aquí -->
+            </select>
+
+            <input type="submit" value="Enviar Pedido">
+        </form>
+    </div>
 </body>
 </html>
