63e0aa2ba2082c1bbd823b84d9a55745c7f9234a
Lucas <66850779+Indaclouds@users.noreply.github.com>
2023-11-30 23:25:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
 <body>
     <h1>Site en Construction depuis 10000 ans !</h1>
     <p>Outer Wilds Ventures travail dur pour améliorer votre expérience. Revenez bientôt !</p>
-    <p>En attendant préparez votre voyage en cliquant sur l'image:</p>
+    <p><b>En attendant préparez votre voyage en cliquant sur l'image:</b></p>
     <img id="toggleButton" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/31abb4cb-6088-4e93-85bc-744885028b01/dd9h88u-d384c96c-b1e3-44d7-b639-88946a92cb2e.png/v1/fill/w_998,h_801/outer_wilds_logo_by_justagirlcalledlex_dd9h88u-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODgwIiwicGF0aCI6IlwvZlwvMzFhYmI0Y2ItNjA4OC00ZTkzLTg1YmMtNzQ0ODg1MDI4YjAxXC9kZDloODh1LWQzODRjOTZjLWIxZTMtNDRkNy1iNjM5LTg4OTQ2YTkyY2IyZS5wbmciLCJ3aWR0aCI6Ijw9MTA5NyJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.uudThtKhfgqxRpUCpUf-pemKntRfmJUuFo5Y5HQbfNU" alt="Image Outer Wilds">
 
     <div class="todolist" id="todolist">
