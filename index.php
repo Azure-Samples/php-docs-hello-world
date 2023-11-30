d534ac2fe0d32d8f0428092a62478631999c2876
Lucas <66850779+Indaclouds@users.noreply.github.com>
2023-11-30 21:59:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site en Construction</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            text-align: center;
+            padding: 100px;
+        }
 
-echo "T MOCHE ENCULEEYYYY !";
+        h1 {
+            color: #333;
+        }
+
+        p {
+            color: #555;
+        }
+
+        /* Ajoutez d'autres styles CSS au besoin */
+    </style>
+</head>
+<body>
+    <h1>Site en Construction depuis 10000 ans !</h1>
+    <p>Nous travaillons dur pour améliorer votre expérience. Revenez bientôt !</p>
+</body>
+</html>
