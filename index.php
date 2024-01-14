54611fb0ea1c959187546cad3b47bbbab2bc9649
7serewhswswyft2asdasdasdsa <130290389+7serewhswswyft2asdasdasdsa@users.noreply.github.com>
2024-01-14 11:43:31 +0100

    Update index.php
    a

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
 <?php
 
 echo "Hello from another another World again!";
+<h1>The picture element</h1>
+
+<p>Resize the browser window to load different images.</p>
+
 <picture>
   <source media="(min-width:650px)" srcset="img_pink_flowers.jpg">
   <source media="(min-width:465px)" srcset="img_white_flower.jpg">
   <img src="img_orange_flowers.jpg" alt="Flowers" style="width:auto;">
 </picture>
+
+</body>
+</html>
