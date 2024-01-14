b73800821eee63012258e2313914b29639edacfc
7serewhswswyft2asdasdasdsa <130290389+7serewhswswyft2asdasdasdsa@users.noreply.github.com>
2024-01-14 11:45:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<?php
-
-echo "Hello from another another World again!";
 <h1>The picture element</h1>
 
 <p>Resize the browser window to load different images.</p>
