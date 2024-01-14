68a0752a5109ba5a4be9606f9fdb1ede7480279b
7serewhswswyft2asdasdasdsa <130290389+7serewhswswyft2asdasdasdsa@users.noreply.github.com>
2024-01-14 11:42:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 
-echo "Hello from another World!";
+echo "Hello from another another World again!";
+<picture>
+  <source media="(min-width:650px)" srcset="img_pink_flowers.jpg">
+  <source media="(min-width:465px)" srcset="img_white_flower.jpg">
+  <img src="img_orange_flowers.jpg" alt="Flowers" style="width:auto;">
+</picture>
