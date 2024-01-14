c498c1fafcc0859f81967ce2b8a69a141ca814d9
7serewhswswyft2asdasdasdsa <130290389+7serewhswswyft2asdasdasdsa@users.noreply.github.com>
2024-01-14 11:50:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,3 @@
 <?php
 
 echo "Hello how are youit ddddddddd |||||||||||||||||||||||||||||||||||||||||||World!";
-
-echo '<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-
-<h1>The picture element</h1>
-
-<p>Resize the browser window to load different images.</p>
-
-<picture>
-  <source media="(min-width:650px)" srcset="img_pink_flowers.jpg">
-  <source media="(min-width:465px)" srcset="img_white_flower.jpg">
-  <img src="img_orange_flowers.jpg" alt="Flowers" style="width:auto;">
-</picture>
-
-</body>
-</html>';
-?>
